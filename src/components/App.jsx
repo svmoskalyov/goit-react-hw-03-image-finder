b65cd0f1b5a42d720fd7ab3e45f371328fd0e9ad
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-11-03 09:59:32 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import Container from './Container';
+import Searchbar from './Searchbar';
+import ImageGalleryInfo from './ImageGalleryInfo';
+
+class App extends Component {
+  state = {
+    imageName: '',
+  };
+
+  handleSearchForm = imageName => {
+    this.setState({ imageName });
+  };
+
+  render() {
+    const { imageName } = this.state;
+
+    return (
+      <Container>
+        <Searchbar onSubmit={this.handleSearchForm} />
+        <ImageGalleryInfo
+          imageName={imageName}
+          currentPage={this.props.currentPage}
+        />
+        <ToastContainer
+          autoClose={2000}
+          theme="colored"
+          position="top-center"
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
