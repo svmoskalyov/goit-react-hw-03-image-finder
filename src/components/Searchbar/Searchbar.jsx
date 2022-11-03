96895515562bc8d619a5b88ad6e27fc5a49c2b81
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-11-03 10:03:47 +0200

    Searchbar

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+// import { Search } from 'react-icons/md';
+import { MdSearch } from 'react-icons/md';
+import styles from './Searchbar.module.css';
+
+class Searchbar extends Component {
+  state = {
+    imageName: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({ imageName: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.imageName.trim() === '') {
+      toast.warn('Please, enter necessary information!');
+      return;
+    }
+
+    this.props.onSubmit(this.state.imageName);
+    this.setState({ imageName: '' });
+  };
+
+  render() {
+    return (
+      <header className={styles.Searchbar}>
+        <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={styles.SearchFormButton}>
+            <MdSearch className={styles.materialIcons} />
+          </button>
+          <input
+            className={styles.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            name="imageName"
+            value={this.state.imageName}
+            onChange={this.handleNameChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
